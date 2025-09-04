0966e4c4a94828d5e9d2626854ba6e35095e4686
Mariana Vélez Múnera <86741870+marianavelezmunera@users.noreply.github.com>
2025-09-03 20:14:30 -0600

    Final modifications before submission
    Final modifications before submission

--- R/06.1-BetaBacteria.R
+++ R/06.1-BetaBacteria.R
@@ -1,142 +1,99 @@
 # Beta diversity 
 
-# Matrix from QIIME2
+# Matrix imported from QIIME
+# UniFrac
+unifrac_hongos<-read_qza("unifrac_hongos.qza")
+matriz_unifrac_hongos<-as.matrix(unifrac_hongos$data)
+unifrac_filosfera_hongos<-matriz_unifrac_hongos[1:15,1:15]
+unifrac_filosfera_hongos<-as.dist(unifrac_filosfera_hongos)
+unifrac_rizosfera_hongos<-matriz_unifrac_hongos[16:29,16:29]
+unifrac_rizosfera_hongos<-as.dist(unifrac_rizosfera_hongos)
+matriz_unifrac_hongos<-as.dist(matriz_unifrac_hongos)
 
-unifrac_bacteria<-read_qza("unifrac_bacteria.qza")
-matriz_unifrac_bacteria<-as.matrix(unifrac_bacteria$data)
-matriz_unifrac_bacteria<-matriz_unifrac_bacteria[1:33,1:33]
-unifrac_phyllosphere_bacteria<-matriz_unifrac_bacteria[1:13,1:13]
-unifrac_phyllosphere_bacteria<-as.dist(unifrac_phyllosphere_bacteria)
-unifrac_rhizosphere_bacteria<-matriz_unifrac_bacteria[14:28,14:28]
-unifrac_rhizosphere_bacteria<-as.dist(unifrac_rhizosphere_bacteria)
-matriz_unifrac_bacteria<-as.dist(matriz_unifrac_bacteria)
+# Weighted UniFrac
+wunifrac_hongos<-read_qza("wunifrac_hongos.qza")
+matriz_wunifrac_hongos<-as.matrix(wunifrac_hongos$data)
+wunifrac_filosfera_hongos<-matriz_wunifrac_hongos[1:15,1:15]
+wunifrac_filosfera_hongos<-as.dist(wunifrac_filosfera_hongos)
+wunifrac_rizosfera_hongos<-matriz_wunifrac_hongos[16:29,16:29]
+wunifrac_rizosfera_hongos<-as.dist(wunifrac_rizosfera_hongos)
+matriz_wunifrac_hongos<-as.dist(matriz_wunifrac_hongos)
 
+# Ordinate
 
-wunifrac_bacteria<-read_qza("wunifrac_bacteria.qza")
-matriz_wunifrac_bacteria<-as.matrix(wunifrac_bacteria$data)
-wunifrac_phyllosphere_bacteria<-matriz_wunifrac_bacteria[1:13,1:13]
-wunifrac_phyllosphere_bacteria<-as.dist(wunifrac_phyllosphere_bacteria)
-wunifrac_rhizosphere_bacteria<-matriz_wunifrac_bacteria[14:28,14:28]
-wunifrac_rhizosphere_bacteria<-as.dist(wunifrac_rhizosphere_bacteria)
-matriz_wunifrac_bacteria<-as.dist(matriz_wunifrac_bacteria)
+pcoa_unifrac_hongos<-ordinate(hongos_rare,method = "PCoA",distance = matriz_unifrac_hongos)
+pcoa_wunifrac_hongos<-ordinate(hongos_rare,method = "PCoA",distance = matriz_wunifrac_hongos)
 
-# Ordinate complete
-
-#UniFrac
-pcoa_unifrac_bacteria<-ordinate(bacteria_rare,method = "PCoA",distance = matriz_unifrac_bacteria)
-
-#Weighted UniFrac
-pcoa_wunifrac_bacteria<-ordinate(bacteria_rare,method = "PCoA",distance = matriz_wunifrac_bacteria) 
-
-# Ordinate by sample type
-
-# Phyllosphere
-pcoa_unifrac_bacteria_phyllosphere<-ordinate(bacteria_phyllosphere,method = "PCoA",distance = unifrac_phyllosphere_bacteria) #UniFrac
-pcoa_wunifrac_bacteria_phyllosphere<-ordinate(bacteria_phyllosphere,method = "PCoA",distance = wunifrac_phyllosphere_bacteria) #Weighted UniFrac
-
-#Rhizosphere
-pcoa_unifrac_bacteria_rhizosphere<-ordinate(bacteria_rhizosphere,method = "PCoA",distance = unifrac_rhizosphere_bacteria) #UniFrac
-pcoa_wunifrac_bacteria_rhizosphere<-ordinate(bacteria_rhizosphere,method = "PCoA",distance = wunifrac_rhizosphere_bacteria) #Weighted
-
-
-#Plots sample type + elevation
-
+# Plots
 # Unweighted UniFrac
 
-plot_ordination(bacteria_rare,pcoa_unifrac_bacteria,shape = "Sample_type",color="Elevation")+
+plot_ordination(hongos_rare,pcoa_unifrac_hongos,shape = "Tipo_muestra",color="Altitud")+
   theme_biome_utils()+
   theme(legend.position = "right")+
-  scale_color_manual(name="Elevation",values=colores)+
+  scale_color_manual(name="Elevation",values=moma.colors("Warhol",5))+
   scale_shape(name="Sample type")
 
 # Weighted UniFrac
 
-plot_ordination(bacteria_rare,pcoa_wunifrac_bacteria,shape = "Sample_type",color="Elevation")+
+plot_ordination(hongos_rare,pcoa_wunifrac_hongos,shape = "Tipo_muestra",color="Altitud")+
   theme_biome_utils()+
   theme(legend.position = "right")+
-  scale_color_manual(name="Elevation",values=colores)+
+  scale_color_manual(name="Elevation",values=moma.colors("Warhol",5))+
   scale_shape(name="Sample type")
-
-# Plots by sample type
-
-# Phyllosphere
-
-#UniFrac
-plot_ordination(bacteria_phyllosphere,pcoa_unifrac_bacteria_phyllosphere,color="Elevation")+
-  theme_biome_utils()+
-  theme(legend.position = "right")+
-  scale_color_manual(name="Elevation",values=colores)+
-  ggtitle("a. Bacterial phyllosphere")+
-  geom_point(size=5)+
-  theme(plot.title = element_text(size=16))+
-  theme(axis.title = element_text(size=16))+
-  theme(legend.title = element_text(size=12))+
-  theme(legend.text = element_text(size=12))
-
-#Weighted UniFrac
-plot_ordination(bacteria_phyllosphere,pcoa_wunifrac_bacteria_phyllosphere,color="Elevation")+
-  theme_biome_utils()+
-  theme(legend.position = "right")+
-  scale_color_manual(name="Elevation",values=colores)+
-  ggtitle("a. Bacterial phyllosphere")+
-  geom_point(size=5)+
-  theme(plot.title = element_text(size=16))+
-  theme(axis.title = element_text(size=16))+
-  theme(legend.title = element_text(size=12))+
-  theme(legend.text = element_text(size=12))
-
-#Rhizosphere 
-
-#UniFrac
-plot_ordination(bacteria_rhizosphere,pcoa_unifrac_bacteria_rhizosphere,color="Elevation")+
-  theme_biome_utils()+
-  theme(legend.position = "right")+
-  scale_color_manual(name="Elevation",values=colores)+
-  ggtitle("b. Bacterial rhizosphere")+
-  geom_point(size=5)+
-  theme(plot.title = element_text(size=16))+
-  theme(axis.title = element_text(size=16))+
-  theme(legend.title = element_text(size=12))+
-  theme(legend.text = element_text(size=12))
-
-#Weighted UniFrac
-plot_ordination(bacteria_rhizosphere,pcoa_wunifrac_bacteria_rhizosphere,color="Elevation")+
-  theme_biome_utils()+
-  theme(legend.position = "right")+
-  scale_color_manual(name="Elevation",values=colores)+
-  ggtitle("b. Bacterial rhizosphere")+
-  geom_point(size=5)+
-  theme(plot.title = element_text(size=16))+
-  theme(axis.title = element_text(size=16))+
-  theme(legend.title = element_text(size=12))+
-  theme(legend.text = element_text(size=12))
-
-#Statistical analysis
 
 #PERMANOVAs
 
-adonis2(matriz_unifrac_bacteria ~ Sample_type*Elevation, data = metadata_bacteria[1:33,])
+adonis2(matriz_unifrac_hongos ~ Tipo_muestra*Altitud, by="terms",data = metadatos_hongos[1:34,])
 
-adonis2(matriz_wunifrac_bacteria~Sample_type*Elevation,data = metadata_bacteria[1:33,])
+adonis2(matriz_wunifrac_hongos~Tipo_muestra*Altitud,by="terms",data = metadatos_hongos[1:34,])
 
 # ANOSIMs 
 
-#UniFrac
-
+# UniFrac
 # Phyllosphere
-anosim(unifrac_phyllosphere_bacteria,subset(metadata_bacteria,Sample_type=="Phyllosphere")$Elevation,permutations = 9999)
+anosim(unifrac_filosfera_hongos,subset(metadatos_hongos,Tipo_muestra=="Filosfera")$Altitud,permutations = 9999)
 
 # Rhizosphere
-anosim(unifrac_rhizosphere_bacteria,subset(metadata_bacteria,Sample_type=="Rhizosphere")$Elevation,permutations = 9999)
+anosim(unifrac_rizosfera_hongos,subset(metadatos_hongos,Tipo_muestra=="Rizosfera")$Altitud,distance = "bray",permutations = 9999)
 
-
-# Weighted UniFrac
+# Wunifrac
 
 # Phyllosphere
-anosim(wunifrac_phyllosphere_bacteria,subset(metadata_bacteria,Sample_type=="Phyllosphere")$Elevation,permutations = 9999)
+anosim(wunifrac_filosfera_hongos,subset(metadatos_hongos,Tipo_muestra=="Filosfera")$Altitud,distance = "bray",permutations = 9999)
 
 # Rhizosphere
-anosim(wunifrac_rhizosphere_bacteria,subset(metadata_bacteria,Sample_type=="Rhizosphere")$Elevation,permutations = 9999)
+anosim(wunifrac_rizosfera_hongos,subset(metadatos_hongos,Tipo_muestra=="Rizosfera")$Altitud,distance = "bray",permutations = 9999)
 
+# Ordination plots by sample type
 
+# Phyllosphere
+# Ordination
+pcoa_unifrac_bacterias_filosfera<-ordinate(bacterias_filosfera,method = "PCoA",distance = unifrac_filosfera_bacterias)
+pcoa_wunifrac_bacterias_filosfera<-ordinate(bacterias_filosfera,method = "PCoA",distance = wunifrac_filosfera_bacterias)
 
+# Plots
+plot_ordination(bacterias_filosfera,pcoa_unifrac_bacterias_filosfera,color="Altitud")+
+  theme_biome_utils()+
+  theme(legend.position = "right")+
+  scale_color_manual(name="Elevation",values=moma.colors("Warhol",5))
+
+plot_ordination(bacterias_filosfera,pcoa_wunifrac_bacterias_filosfera,color="Altitud")+
+  theme_biome_utils()+
+  theme(legend.position = "right")+
+  scale_color_manual(name="Elevation",values=moma.colors("Warhol",5))
+
+#Rhizosphere
+# Ordination
+pcoa_unifrac_bacterias_rizosfera<-ordinate(bacterias_rizosfera,method = "PCoA",distance = unifrac_rizosfera_bacterias)
+pcoa_wunifrac_bacterias_rizosfera<-ordinate(bacterias_rizosfera,method = "PCoA",distance = wunifrac_rizosfera_bacterias)
+
+# Plots
+plot_ordination(bacterias_rizosfera,pcoa_unifrac_bacterias_rizosfera,color="Altitud")+
+  theme_biome_utils()+
+  theme(legend.position = "right")+
+  scale_color_manual(name="Elevation",values=moma.colors("Warhol",5))
+
+plot_ordination(bacterias_rizosfera,pcoa_wunifrac_bacterias_rizosfera,color="Altitud")+
+  theme_biome_utils()+
+  theme(legend.position = "right")+
+  scale_color_manual(name="Elevation",values=moma.colors("Warhol",5))
