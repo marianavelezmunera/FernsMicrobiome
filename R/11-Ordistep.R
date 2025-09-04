d03fa7513b2871b2f9a194fc8cce4d11a7057a0f
Mariana Vélez Múnera <86741870+marianavelezmunera@users.noreply.github.com>
2025-09-03 20:48:52 -0600

    Final modifications before submission
    Final modifications before submission

--- R/11-Ordistep.R
+++
@@ -1,4 +0,0 @@
-ordistep(rda_hongos_bodysize_rizo_data, perm.max = 200) 
-ordistep(rda_hongos_bodysize_filo_data,perm.max=200)
-ordistep(rda_bacterias_bodysize_filo_data,perm.max=200)
-ordistep(rda_bacterias_bodysize_rizo_data, perm.max = 200)
