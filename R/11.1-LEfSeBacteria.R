8959c40eca80e9c37da674db4316c631d9447116
Mariana Vélez Múnera <86741870+marianavelezmunera@users.noreply.github.com>
2025-09-03 20:33:08 -0600

    Final modifications before submission
    Final modifications before submission

--- R/11.1-LEfSeBacteria.R
+++ R/11.1-LEfSeBacteria.R
@@ -1,26 +1,13 @@
 #LefSe bacteria
 
-#Phyllo
+#Phyllosphere
 
-
-bacteria_phyllo_lefse<-trans_diff$new(bacteria_meco_phyllo,method = "lefse",group = "Elevation",alpha = 0.05,p_adjust_method = "none")
-
-
-lefse_plot_bacteria_phyllo <-bacteria_phyllo_lefse$plot_diff_bar(group_order = c("1978", "2007", "2018","2178","2210"),color_values = colores,threshold = 3,keep_full_name = FALSE)
-
-lefse_plot_bacteria_phyllo<-lefse_plot_bacteria_phyllo+
-  geom_bar(color="black",stat = "identity")+
-  ggtitle("c. Fungal phyllosphere")+
-  theme(legend.position = "none")
-
+bacterias_filo_lefse<-trans_diff$new(bacterias_meco_filo,method = "lefse",group = "Altitud",alpha = 0.05,p_adjust_method = "none")
+# Only keeping genera with LDA>3
+lefse_bacterias_filo_genus<-bacterias_filo_lefse$res_diff %>% as_tibble() %>% separate(Taxa, into = c("k","p","c","o","f","g","s"), sep="\\|",extra="merge") %>% filter(!is.na(g)) %>% filter(is.na(s)) %>% filter(LDA>=3)
 
 # Rhizosphere
 
-bacteria_rhizo_lefse<-trans_diff$new(bacteria_meco_rhizo,method = "lefse",group = "Elevation",alpha = 0.05,p_adjust_method = "none")
-
-lefse_plot_bacteria_rhizo<-bacteria_rhizo_lefse$plot_diff_bar(group_order = c("1978", "2007", "2018","2178","2210"),color_values = c(colores[1],colores[2],colores[4],colores[3],colores[5]),threshold = 3,keep_full_name = FALSE)
-
-lefse_plot_bacteria_rhizo<-lefse_plot_bacteria_rhizo+
-  ggtitle("d. Fungal rhizosphere")+
-  theme(legend.position = "none")+
-  geom_bar(color="black",stat = "identity")
+bacterias_rizo_lefse<-trans_diff$new(bacterias_meco_rizo,method = "lefse",group = "Altitud",alpha = 0.05,p_adjust_method = "none")
+# Only keeping genera with LDA>3
+lefse_bacterias_rizo_genus<-bacterias_rizo_lefse$res_diff %>% as_tibble() %>% separate(Taxa, into = c("k","p","c","o","f","g","s"), sep="\\|",extra="merge") %>% filter(!is.na(g)) %>% filter(is.na(s)) %>% filter(LDA>=3)
