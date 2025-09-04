f48c1c0a1fd769bed748f0d6c32a6c64937336ce
Mariana Vélez Múnera <86741870+marianavelezmunera@users.noreply.github.com>
2025-09-03 20:20:02 -0600

    Final modifications before submission
    Final modifications before submission

--- R/07.1-TaxonomyBacteria.R
+++ R/07.1-TaxonomyBacteria.R
@@ -1,38 +1,16 @@
 # Taxonomy 
 
-#Object for the plot
+## Data 
 
+bacterias_rare_taxo<-bacterias_rare
+rownames(bacterias_rare_taxo@sam_data)<-paste0(bacterias_rare_taxo@sam_data$Altitud,sep="_",bacterias_rare_taxo@sam_data$id)
+sample_names(bacterias_rare_taxo)<-paste0(bacterias_rare_taxo@sam_data$Altitud,sep="_",bacterias_rare_taxo@sam_data$id)
+bacterias_rare_taxo@sam_data$id<-paste0(bacterias_rare_taxo@sam_data$Altitud,sep="_",bacterias_rare_taxo@sam_data$id)
 
-bacteria_rare_taxo<-bacteria_rare
-rownames(bacteria_rare_taxo@sam_data)<-paste0(bacteria_rare_taxo@sam_data$Elevation,sep="_",bacteria_rare_taxo@sam_data$id)
+# As compositional
+# By Class
+total_bacterias<-microbiome::transform(aggregate_top_taxa2(subset_taxa(bacterias_rare_taxo,!is.na(Class)),"Class",top=10),"compositional")
 
-sample_names(bacteria_rare_taxo)<-paste0(bacteria_rare_taxo@sam_data$Elevation,sep="_",bacteria_rare_taxo@sam_data$id)
-
-bacteria_rare_taxo@sam_data$id<-paste0(bacteria_rare_taxo@sam_data$Elevation,sep="_",bacteria_rare_taxo@sam_data$id)
-
-
-total_bacteria<-microbiome::transform(aggregate_top_taxa2(subset_taxa(bacteria_rare_taxo,!is.na(Class)),"Class",top=10),"compositional")
-
-# Plot
-
-total_bacteria@sam_data$label<-c("2210","2210","2210","1978","1978","1978","2178","2178","2178","2007","2007","2007","2018",
-                                 "2210","2210","2210","1978","1978","1978","2178","2178","2178","2007","2007","2007","2018","2018",
-                                 "2018","2210","1978","2178","2007","2018") #Labels for the plot
-
-bacteria_taxonomy_plot<-plot_composition(total_bacteria,group_by = "Sample_type", sample.sort = "Elevation",x.label = "label")+
-  theme_biome_utils()+
-  theme(legend.position = "right")+
-  scale_fill_manual(name="Class",values=moma.colors("Warhol",21),labels=c("Acidimicrobiia","Terriglobia","Actinomycetes","Alphaproteobacteria",
-                                                                          "Bacteroidia","Vulcanimicrobiia","Gammaproteobacteria","Other",
-                                                                          "Planctomycetia","Polyangiia","Verrucomicrobiia"))+
-  ylab("Relative abundance")+
-  xlab("Sample")+
-  theme(axis.text = element_text(angle = 90))+
-  theme(strip.text = element_text(size = 14))+
-  theme(axis.title = element_text(size=14))+
-  theme(legend.title = element_text(size=12))+
-  theme(legend.text = element_text(size=12))+
-  ggtitle("a. Bacteria")+
-  theme(plot.title = element_text(size=14))+
-  geom_bar(stat = "identity",color="black")
-bacteria_taxonomy_plot
+# By Phylum
+filos_bacterias<-microbiome::transform(aggregate_top_taxa2(subset_taxa(bacterias_rare_taxo,!is.na(Phylum)),"Phylum",top=10),"compositional")
+filos_bacterias@otu_table
