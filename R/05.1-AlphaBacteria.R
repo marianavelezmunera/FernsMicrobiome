ffc4192538ed139e2b0c1f6d9d36dc2983958069
Mariana Vélez Múnera <86741870+marianavelezmunera@users.noreply.github.com>
2025-09-03 20:09:00 -0600

    Final modifications before submission
    Final modifications before submission

--- R/05.1-AlphaBacteria.R
+++ R/05.1-AlphaBacteria.R
@@ -1,61 +1,53 @@
 # Alpha diversity
 
-diversity_alpha_bacteria<-alpha(bacteria_rare,index = "all") 
-diversity_alpha_bacteria$id<-rownames(diversity_alpha_bacteria)
+diversidad_alfa_bacterias<-alpha(bacterias_rare,index = "all") 
+faith_pd_bacterias<-read_qza("faith_pd_vector_bacterias.qza")
+FaithsPD_bacterias<-as.data.frame(faith_pd_bacterias$data)
+FaithsPD_bacterias$id<-rownames(FaithsPD_bacterias)
+diversidad_alfa_bacterias$id<-rownames(diversidad_alfa_bacterias)
+diversidad_alfa_bacterias<-left_join(diversidad_alfa_bacterias,FaithsPD_bacterias,by="id")
 
-diversity_alpha_bacteria<-left_join(metadata_bacteria[1:34,1:5],diversity_alpha_bacteria,by="id")
+
+diversidad_alfa_bacterias<-left_join(metadatos_bacterias[1:34,1:5],diversidad_alfa_bacterias,by="id")
+
+colnames(diversidad_alfa_bacterias)[28]<-"faith_pd"
+diversidad_alfa_bacterias<-diversidad_alfa_bacterias[-34,]
 
 # Plots
 
-# Linnear models to find abline
-
-# Phyllosphere bacteria
-
-diversity_alpha_bacteria %>% as_tibble() %>%
-  mutate(Elevation = factor(Elevation,levels= c(1978,2007,2018,2178,2210)))%>%
-  mutate(Elevation3 = as.numeric(Elevation),.before=Elevation) %>%
-  filter(Sample_type=="Phyllosphere") %>%
-  lm(diversity_shannon ~ Elevation3,data=.) %>% summary(.)
-
-
-# Rhizosphere bacteria
-diversity_alpha_bacteria %>% as_tibble() %>%
-  mutate(Elevation = factor(Elevation,levels= c(1978,2007,2018,2178,2210)))%>%
-  mutate(Elevation3 = as.numeric(Elevation),.before=Elevation) %>%
-  filter(Sample_type=="Rhizosphere") %>%
-  lm(diversity_shannon ~ Elevation3,data=.) %>% summary(.)
-
-shannon_bacteria_phyllo<-ggplot(data = subset(diversity_alpha_bacteria,Sample_type=="Phyllosphere"),aes(x=Elevation, y=diversity_shannon,fill=Elevation))+
-  geom_point(aes(fill=Elevation),shape=21,size=5,colour="black")+
+# Shannon vs elevation 
+# Phyllosphere
+ggplot(data = subset(diversidad_alfa_bacterias,Tipo_muestra=="Filosfera"),aes(x=Altitud, y=diversity_shannon,fill=Altitud))+
+  geom_boxplot(color="black")+
   theme_biome_utils()+
-  xlab("Elevation (masl)")+ylab("Shannon Index (H)") +
+  xlab("Elevation")+ylab("H") +
   theme(legend.position = "none")+
-  scale_fill_manual(values=colores)+
-  geom_abline(intercept =4.96658,slope = -0.10095)+
-  ggtitle("a.Bacterial phyllosphere")+
-  theme(plot.title = element_text(size=16))+
-  theme(axis.title = element_text(size=16))
-shannon_bacteria_phyllo
-
-shannon_bacteria_rhizo<-ggplot(data = subset(diversity_alpha_bacteria,Sample_type=="Rhizosphere"),aes(x=Elevation, y=diversity_shannon,fill=Elevation))+
-  geom_point(aes(fill=Elevation),shape=21,size=5,colour="black")+
+  scale_fill_manual(values=moma.colors("Warhol",5))
+# Rhizosphere
+ggplot(data = subset(diversidad_alfa_bacterias,Tipo_muestra=="Rizosfera"),aes(x=Altitud, y=diversity_shannon,fill=Altitud))+
+  geom_boxplot(color="black")+
   theme_biome_utils()+
-  xlab("Elevation (masl)")+ylab("Shannon Index (H)") +
+  xlab("Elevation")+ylab("H") +
   theme(legend.position = "none")+
-  scale_fill_manual(values=colores)+
-  geom_abline(intercept =7.14814,slope = -0.06647)+
-  ggtitle("b. Bacterial rhizosphere")+
-  theme(plot.title = element_text(size=16))+
-  theme(axis.title = element_text(size=16))
-shannon_bacteria_rhizo
+  scale_fill_manual(values=moma.colors("Warhol",5))
+
+# Full plots (elevation+sample type)
+
+ggplot(data = diversidad_alfa_bacterias,aes(x=Altitud,y=diversity_shannon,fill=Tipo_muestra))+
+  geom_boxplot(color="black")+
+  theme_biome_utils()+
+  xlab("Elevation")+ylab("H")+
+  theme(legend.position = "bottom")+
+  scale_fill_manual(name="Tipo de muestra", label=c("Phyllosphere","Rhizosphere","Bulk soil"),values=moma.colors("Warhol",3))
 
 ## Statistical analysis
 
 # Two way ANOVA Shannon
 
-anova_shannon_bacteria<-aov(diversity_shannon~Sample_type*Elevation,data = diversity_alpha_bacteria)
-summary(anova_shannon_bacteria)
+anova_shannon_bacterias<-aov(diversity_shannon~Tipo_muestra*Altitud,data = diversidad_alfa_bacterias)
+summary(anova_shannon_bacterias2)
+TukeyHSD(anova_shannon_bacterias)
 
-
-anova_shannon_bacteria<-aov(diversity_shannon~Sample_type*Elevation,data = diversity_alpha_bacteria)
-summary(anova_shannon_bacteria)
+anova_shannon_bacterias2<-aov(diversity_shannon~Tipo_muestra,data = diversidad_alfa_bacterias)
+summary(anova_shannon_bacterias)
+TukeyHSD(anova_shannon_bacterias)
